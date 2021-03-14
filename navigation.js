35d782fdf8fa88dec035abd74666be9d37e9e2f3
jeremysteffen <74683309+jeremysteffen@users.noreply.github.com>
2021-03-13 19:19:37 -0700

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReplyButton from './reply-button';
+import LikeButton from './like-button';
+import UserNameButton from './username-button';
+import PasswordButton from './password-button';
+import LoginButton from './login-button';
+
+let e = React.createElement;
+
+export default class Comment extends React.Component {
+    render() {
+        return (
+            <div className="card w-75">
+                <div className="card-header bg-success text-white">
+                    Username and Time
+                </div>
+                <div className="card-body">
+                    Comment content
+                </div>
+                <div className="card-footer">
+                    <LikeButton />
+                    <ReplyButton />
+                    <UserNameButton />
+                    <PasswordButton />
+                    <LoginButton />
+                </div>
+            </div>
+        );
+    }
+}
---
+++ news-feed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './post';
+
+let e = React.createElement;
+
+export default class NewsFeed extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Post />
+                <Post />
+            </div>
+        );
+    }
+}
